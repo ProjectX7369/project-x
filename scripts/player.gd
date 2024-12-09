046e8ee9d7a2ea0a747fa6b7bef35958c83795ac
ProjectX7369 <project.x.7369@gmail.com>
2024-12-08 19:07:32 -0500

    Changes and More
    Adding models

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,7 +3,7 @@ extends CharacterBody3D
 @export_group("Camera")
 @export_range(0.0, 1.0) var mouse_sensitivity := 0.25
 @export var tilt_upper_limit := PI / 3.0
-@export var tilt_lower_limit := -0.1
+@export var tilt_lower_limit := -PI / 8.0
 @export var zoom_minimum = 8
 @export var zoom_maximum = 2
 
@@ -33,6 +33,7 @@ var jump_double = true
 
 
 func _ready() -> void:
+	print("funcionando")
 	_animation_player.stop()
 	
 func _input(event: InputEvent) -> void:
